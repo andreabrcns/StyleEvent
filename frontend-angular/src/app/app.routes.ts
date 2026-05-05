8c9624e1164ce425a87b37caf1eecc7fcd0f39d5
andreabrcns <u3761506554@gmail.com>
2026-05-05 10:51:37 +0200

    Routing configurado

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Catalogo } from './pages/catalogo/catalogo';
+import { DetalleVestido } from './pages/detalle-vestido/detalle-vestido';
+import { Login } from './pages/login/login';
+import { Registro } from './pages/registro/registro';
+import { Favoritos } from './pages/favoritos/favoritos';
+import { Admin } from './pages/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'catalogo', component: Catalogo },
+    { path: 'detalle', component: DetalleVestido },
+    { path: 'login', component: Login },
+    { path: 'registro', component: Registro },
+    { path: 'favoritos', component: Favoritos },
+    { path: 'admin', component: Admin }
+];
