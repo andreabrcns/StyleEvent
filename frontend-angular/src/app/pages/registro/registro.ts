11c8decdfd07371c4ad6910b4f431fc3d06dc9ba
andreabrcns <u3761506554@gmail.com>
2026-05-19 12:52:26 +0200

    Login y panel de admin hecho

--- frontend-angular/src/app/pages/registro/registro.ts
+++ frontend-angular/src/app/pages/registro/registro.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
-export class Registro {}
+export class Registro { }
